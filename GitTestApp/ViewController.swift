131369027e18d5666c041222e87d656b66c432f7
Uğur Çetinel <ugurcetinel57@gmail.com>
2021-01-26 23:38:06 +0300

    Yorum eklendi  deneme

---
+++ GitTestApp/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  GitTestApp
+//
+//  Created by Uğur Çetinel on 26.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("aaaaa")
+        print("aaaaa")
+        
+        //dfjkbsdcvkjbsdlvjbsljkvbksjd
+
+    }
+
+
+}
+
